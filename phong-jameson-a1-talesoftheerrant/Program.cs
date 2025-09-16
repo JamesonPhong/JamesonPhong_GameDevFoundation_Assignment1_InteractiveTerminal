e259bb0c39eab1070b4e027e6191b4b9628b7747
JamesonPhong <000882371@mohawkcollege.ca>
2025-09-16 01:14:02 -0400

    Add Sub-Class choice.

--- phong-jameson-a1-talesoftheerrant/Program.cs
+++ phong-jameson-a1-talesoftheerrant/Program.cs
@@ -52,14 +52,80 @@ string classChoice = Console.ReadLine();
 if (classChoice.ToUpper() == "COMBAT")
 {
     Console.WriteLine("A Combatant! A master of physical prowess and weaponry.");
+
+    // Seperator Space
+    Console.WriteLine("");
+
+    // Choosing your Combat Sub-Class
+    Console.WriteLine("What type of Combatant are you?");
+    Console.WriteLine("[KNIGHT] [BARBARIAN]");
+
+    string subClassChoice = Console.ReadLine();
+    if (subClassChoice.ToUpper() == "KNIGHT")
+    {
+        Console.WriteLine("A Knight! A noble warrior bound by honor and chivalry.");
+    }
+    else if (subClassChoice.ToUpper() == "BARBARIAN")
+    {
+        Console.WriteLine("A Barbarian! A fierce fighter fueled by rage and primal strength.");
+    }
+    else
+    {
+        Console.WriteLine("Hm, still can't remember? Give it a bit more time to come back to you.");
+        subClassChoice = Console.ReadLine();
+    }
 }
 else if (classChoice.ToUpper() == "MAGIC")
 {
     Console.WriteLine("A Mage! A wielder of arcane powers and mystical arts.");
+
+    // Seperator Space
+    Console.WriteLine("");
+
+    // Choosing your Magic Sub-Class
+    Console.WriteLine("What type of Mage are you?");
+    Console.WriteLine("[SUMMONER] [BISHOP]");
+
+    string subClassChoice = Console.ReadLine();
+    if (subClassChoice.ToUpper() == "SUMMONER")
+    {
+        Console.WriteLine("A Summoner! A conjurer of otherworldly beings to aid in battle.");
+    }
+    else if (subClassChoice.ToUpper() == "BISHOP")
+    {
+        Console.WriteLine("A Bishop! A healer and protector, channeling divine energy.");
+    }
+    else
+    {
+        Console.WriteLine("Hm, still can't remember? Give it a bit more time to come back to you.");
+        subClassChoice = Console.ReadLine();
+    }
 }
 else if (classChoice.ToUpper() == "STEALTH")
 {
     Console.WriteLine("A Rogue! A shadowy figure skilled in subterfuge and agility.");
+
+    // Seperator Space
+    Console.WriteLine("");
+
+    // Choosing your Stealth Sub-Class
+    Console.WriteLine("What type of Rogue are you?");
+    Console.WriteLine("[THIEF] [ASSASSIN]");
+
+    string subClassChoice = Console.ReadLine();
+    if (subClassChoice.ToUpper() == "THIEF")
+    {
+        Console.WriteLine("A Thief! A cunning individual adept at stealth and lockpicking.");
+    }
+    else if (subClassChoice.ToUpper() == "ASSASSIN")
+    {
+        Console.WriteLine("An Assassin! A deadly operative skilled in silent takedowns.");
+    }
+    else
+    {
+        Console.WriteLine("Hm, still can't remember? Give it a bit more time to come back to you.");
+        subClassChoice = Console.ReadLine();
+    }
 }
 else
 {
