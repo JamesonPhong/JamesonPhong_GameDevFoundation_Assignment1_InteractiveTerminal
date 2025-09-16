c3726135c28130786b4986b0706f1a70076a2b89
JamesonPhong <000882371@mohawkcollege.ca>
2025-09-16 01:00:01 -0400

    Update Introduction. Add Race Choice.

--- phong-jameson-a1-talesoftheerrant/Program.cs
+++ phong-jameson-a1-talesoftheerrant/Program.cs
@@ -6,7 +6,35 @@ Console.WriteLine("Greetings Lost Soul of the Void.");
 Console.WriteLine("I am Arla, Goddess of Rebirth");
 Console.WriteLine("I have chosen you to embark on a perilous journey.");
 Console.WriteLine("The land of Lodenhal Reach is being invaded by Demons of the Blood-Rift.");
-Console.WriteLine("I sense greatness in you, Lost Soul of the Void. You are the Beacon of Light the people of Lodenhal Reach have been praying for.");
-Console.WriteLine("Unfortunately, your mortal body has long been forgotten to the sands of time. But that is why I have... Borrowed the All-Seeing Hourglass from Kronos... Don't tell him.");
+Console.WriteLine("I sense greatness in you, Lost Soul of the Void.");
+Console.WriteLine("You are the Beacon of Light the people of Lodenhal Reach have been praying for.");
+Console.WriteLine("Unfortunately, your mortal body has long been forgotten to the sands of time.");
+Console.WriteLine("But that is why I have... Borrowed the All-Seeing Hourglass from Kronos... Don't tell him.");
 Console.WriteLine("With this artifact, you can peer back into time.");
 Console.WriteLine("Although fragmented, the memories of your past life will eventually return.");
+
+// Seperator Space
+Console.WriteLine("");
+
+// Choosing your Race
+Console.WriteLine("Now then Lost Soul of the Void. What Race do you hail from?");
+Console.WriteLine("[HUMAN] [ELF] [BEASTFOLK]");
+
+string race = Console.ReadLine();
+if (race.ToUpper() == "HUMAN")
+{
+    Console.WriteLine("Ah, a Human. A versatile and resilient race.");
+}
+else if (race.ToUpper() == "ELF")
+{
+    Console.WriteLine("Ah, an Elf. A wise and agile race.");
+}
+else if (race.ToUpper() == "BEASTFOLK")
+{
+    Console.WriteLine("Ah, a Beastfolk. A strong and fierce race.");
+}
+else
+{
+    Console.WriteLine("Hm, still can't remember? Give it a bit more time to come back to you.");
+    race = Console.ReadLine();
+}
