7c6d1216abfb9516266ebc029d6ee0da498aac98
JamesonPhong <000882371@mohawkcollege.ca>
2025-09-16 00:42:32 -0400

    Add introduction

--- phong-jameson-a1-talesoftheerrant/Program.cs
+++ phong-jameson-a1-talesoftheerrant/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Introduction to Tales Of The Errant
+Console.WriteLine("[Tales Of The Errant]");
+Console.WriteLine("Greetings Lost Soul of the Void.");
+Console.WriteLine("I am Arla, Goddess of Rebirth");
+Console.WriteLine("I have chosen you to embark on a perilous journey.");
+Console.WriteLine("The land of Lodenhal Reach is being invaded by Demons of the Blood-Rift.");
+Console.WriteLine("I sense greatness in you, Lost Soul of the Void. You are the Beacon of Light the people of Lodenhal Reach have been praying for.");
+Console.WriteLine("Unfortunately, your mortal body has long been forgotten to the sands of time. But that is why I have... Borrowed the All-Seeing Hourglass from Kronos... Don't tell him.");
+Console.WriteLine("With this artifact, you can peer back into time.");
+Console.WriteLine("Although fragmented, the memories of your past life will eventually return.");
